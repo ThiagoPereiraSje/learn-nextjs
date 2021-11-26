3383c4952f70c84ed04c230be277c52c5248b005
Thiago Pereira <thiagopereiramail@gmail.com>
2021-11-26 17:06:35 +0000

    Navigating Programmatically

--- next-routes/pages/index.js
+++ next-routes/pages/index.js
@@ -1,6 +1,15 @@
+import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/product");
+    // router.replace("/product");
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
@@ -10,6 +19,9 @@ export default function Home() {
       <Link href="/product">
         <a>Products</a>
       </Link>
+      <br />
+
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
