1b3ededbbadd21321f2f29049eb79301135ffb09
Thiago Pereira <thiagopereiramail@gmail.com>
2021-11-26 18:21:05 +0000

    Static generation with getStaticProps

---
+++ next-prerendering/static-gen-with-data-fetch/pages/users.jsx
@@ -0,0 +1,30 @@
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  const data = await response.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
+
+// users inside the props
+export default function UserList({ users }) {
+  return (
+    <>
+      <h1>List of users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
