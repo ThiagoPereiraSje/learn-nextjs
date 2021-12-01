0eecf29187b1cf2f79c4664d70bf8fe13fdf07e1
Thiago Pereira <thiagopereiramail@gmail.com>
2021-12-01 19:49:48 +0000

    Data Fetching with Static Generation

--- next-prerendering/static-gen-with-data-fetch/pages/users.jsx
+++ next-prerendering/static-gen-with-data-fetch/pages/users.jsx
@@ -1,3 +1,5 @@
+/* Fetch external data in build time and
+ send inside the props for the page.  */
 export async function getStaticProps() {
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/users"
@@ -5,6 +7,7 @@ export async function getStaticProps() {
 
   const data = await response.json();
 
+  // Send data for the page.
   return {
     props: {
       users: data,
@@ -12,7 +15,7 @@ export async function getStaticProps() {
   };
 }
 
-// users inside the props
+// users inside the props here!
 export default function UserList({ users }) {
   return (
     <>
